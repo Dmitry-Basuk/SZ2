1428af164bd8943c72bf51a5837366fdd42ef2c5
Basuk Dmitry <basukdmity@mail.ru>
2022-12-02 11:36:09 +0300

    добавил пример 3 (рабочий)

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,21 +1,20 @@
 ﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
-
 34, 5 -> не кратно, остаток 4 
 16, 4 -> кратно
 */
-
 Console.WriteLine("Введите первое число");
-int numbreOne = int.Parse(Console.ReadLine()??"");
+int numberOne = int.Parse(Console.ReadLine()??"");
 Console.WriteLine("Введите второе число");
-int numbreTwo = int.Parse(Console.ReadLine()??"");
+int numberTwo = int.Parse(Console.ReadLine()??"");
 
-if (numbreOne % numbreTwo == 0)
+
+
+if (numberOne % numberTwo == 0)
 {
     Console.WriteLine($"Первле число {numberOne} кратно второму {numberTwo}");
 }
 else
 {
-    int ost = numbreOne % numberTwo;
+    int ost = numberOne % numberTwo;
     Console.WriteLine($"Первле число {numberOne} не кратно второму {numberTwo}, остаток от деления {ost}");
 }
-
