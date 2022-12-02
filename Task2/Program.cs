60a47411c1892f3536cddc0ba76ba9d8c676d425
Basuk Dmitry <basukdmity@mail.ru>
2022-12-02 10:31:36 +0300

    Добавил пример 2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+Random rnd = new Random(); // если у нас только левая часть, то по умолчанию там Null
+int number = rnd.Next(100, 1000);
+Console.WriteLine($"Число равно: {number}");
+int hungreds = number / 100;
+int ones = number % 10;
+
+int result = (hungreds * 10) + ones;
+Console.WriteLine($"результат равен {result}");
